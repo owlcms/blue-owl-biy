695f94b97a8513bfbf0fd3056090add815a108a4
Jean-François Lamy <jf@jflamy.dev>
2023-01-17 19:58:18 -0500

    unable to find serialport dependency

--- src/scripts/dialog.ts
+++ src/scripts/dialog.ts
@@ -1,8 +1,17 @@
 import type { MessageBoxOptions } from 'electron';
 import { BrowserWindow, dialog } from '@electron/remote';
+import { doRunner } from './timekeeper';
 
 
 export function hello() {
+    console.warn("allo!");
+    doRunner({
+        serialPort: "COM10",
+        mqttPassword: "test",
+        mqttUrl: "mqtt://127.0.0.1",
+        mqttUsername: "test",
+        platform: "A",
+    });
     const options: MessageBoxOptions = {
         title: 'Some title',
         type: 'info',
@@ -10,9 +19,9 @@ export function hello() {
         message: 'Some message',
         detail: 'hello',
     };
-	const win = BrowserWindow.getFocusedWindow();
+    const win = BrowserWindow.getFocusedWindow();
     if (win) dialog.showMessageBox(win, options);
 }
 
-const button = document.getElementById('btnShowHello')
-if (button) button.onclick = hello;
+const button = document.getElementById('btnShowHello');
+if (button) button.onclick=hello
--- src/scripts/timekeeper.ts
+++ src/scripts/timekeeper.ts
@@ -1,27 +1,33 @@
-import runner from 'scripts/runner';
-import Timekeeper from 'lib/model/timekeeper';
-import timekeeperButtons from 'lib/model/timekeeper/buttons';
+import runner from './runner';
+import Timekeeper from '../lib/model/timekeeper';
+import timekeeperButtons from '../lib/model/timekeeper/buttons';
+import type { Config } from './config';
 
-runner(({
-    board,
-    owlcms,
-    platform,
-}) => {
-    new Timekeeper({
-        modules: [
-            timekeeperButtons({
-                board,
-                oneMinuteButton: 11,
-                oneMinuteButtonPullUp: true,
-                startButton: 9,
-                startButtonPullUp: true,
-                stopButton: 10,
-                stopButtonPullUp: true,
-                twoMinuteButton: 12,
-                twoMinuteButtonPullUp: true,
-            }),
-        ],
-        owlcms,
-        platform,
-    });
-});
+export function doRunner(config?: Config) {
+    console.log("doRunner");
+    runner(
+        ({
+            board,
+            owlcms,
+            platform,
+        }) => {
+            new Timekeeper({
+                modules: [
+                    timekeeperButtons({
+                        board,
+                        oneMinuteButton: 11,
+                        oneMinuteButtonPullUp: true,
+                        startButton: 9,
+                        startButtonPullUp: true,
+                        stopButton: 10,
+                        stopButtonPullUp: true,
+                        twoMinuteButton: 12,
+                        twoMinuteButtonPullUp: true,
+                    }),
+                ],
+                owlcms,
+                platform,
+            });
+        }
+    , config);
+}
